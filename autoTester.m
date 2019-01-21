57b6c6810deb3751401341a7298ddeb36c314823
Nick Schrader <nick.schrader@mailbox.org>
2019-01-22 00:09:59 +0100

    Channel measures

--- autoTester.m
+++ autoTester.m
@@ -1,27 +1,26 @@
-w = warning('query','last');
-id=w.identifier;
-warning('off',id);
+warning('off', 'all');
 
-result = zeros(80,3);
-for j=200:10:1000
-    for i=2:4:100
-        config = "Ts = ";
-        config = config + (0.05e-6*j);
-        config = config + "; dataChannels = ";
-        config = config + (j-i);
-        config = config + "; totalChannels = 1024; driverChannels =";
-        config = config + (i);
-        config = config + "; tau = 100e-9;";
-        set_param('ofdm', 'InitFcn', config);
-        sim('ofdm');
-        if ber.data(end, 1) < 0.02
-            result((j-200)/10+1, 1) = i;
-            result((j-200)/10+1, 2) = 2*(j-i)/(0.05e-6*j)/1e6;
-            result((j-200)/10+1, 3) = ber.data(end, 1);
-            break;
-        end
-    end
-    result((j-200)/10+1,:)
+result = zeros(10,4);
+j = 4096;
+idx = 1;
+for i=[10 16 24 32 48 64 96 128 196 256]
+    config = "Ts = ";
+    config = config + (0.05e-6*j);
+    config = config + "; driverChannels =";
+    config = config + (i);
+    config = config + "; dataChannels = ";
+    config = config + (j-i);
+    config = config + "; totalChannels = ";
+    config = config + 2^ceil(log2(j));
+    config = config + "; tau = 100e-9; SNR=20; M=16;";
+    set_param('ofdm', 'InitFcn', config);
+    sim('ofdm');
+    result(idx, 1) = j;
+    result(idx, 2) = i;
+    result(idx, 3) = 2*(j-i)/(0.05e-6*j)/1e6;
+    result(idx, 4) = ber.data(end, 1);
     save('result', 'result');
+    idx = idx + 1;
 end
-result
+result(idx,:)
+
